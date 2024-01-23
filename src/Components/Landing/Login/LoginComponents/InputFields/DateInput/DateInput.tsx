74516b789d9a886f75fdcf3965ba41b31c7f3973
Brady Stacey <bradyjstacey@gmail.com>
2024-01-23 15:48:29 -0800

    date selects now work properly and are complete

--- src/Components/Landing/Login/LoginComponents/InputFields/DateInput/DateInput.tsx
+++ src/Components/Landing/Login/LoginComponents/InputFields/DateInput/DateInput.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import "./DateInput.css";
 
 interface DateInputProps {
@@ -6,7 +6,14 @@ interface DateInputProps {
 }
 
 export const DateInput: React.FC<DateInputProps> = ({ dateSelect }) => {
-	const selectRef = useRef<HTMLSelectElement>(null);
+	const [inFocus, setInFocus] = useState(false);
+	const days = Array.from({ length: 31 }, (_, i) => i + 1);
+	const endYear = new Date().getFullYear();
+	const startYear = 1900;
+	const years = Array.from(
+		{ length: endYear - startYear + 1 },
+		(_, i) => i + startYear
+	);
 	const months = [
 		"January",
 		"February",
@@ -21,51 +28,94 @@ export const DateInput: React.FC<DateInputProps> = ({ dateSelect }) => {
 		"November",
 		"December",
 	];
-	const openDropdown = () => {
-		if (selectRef.current) {
-			selectRef.current.click();
+
+	const determineDivClass = () => {
+		if (inFocus) {
+			return "date-input-container-blue";
 		}
+		return "date-input-container";
 	};
+
+	const determineLabelClass = () => {
+		if (inFocus) {
+			return "date-input-date-select-blue";
+		}
+		return "date-input-date-select";
+	};
+
+	const handleFocus = () => setInFocus(true);
+	const handleBlur = () => setInFocus(false);
+
 	return (
-		<div className="date-input-container" onClick={openDropdown}>
+		<div className="date-input-container">
 			{dateSelect === "Month" ? (
-				<>
+				<div
+					className={determineDivClass()}
+					onFocus={handleFocus}
+					onBlur={handleBlur}
+				>
 					{" "}
-					<label className="date-input-date-select">{dateSelect}</label>
-					<select ref={selectRef} className="date-input-select">
-						{months.map((month, index) => (
-							<option key={index} value={month}>
-								{month}
-							</option>
-						))}
-					</select>
-				</>
+					<div className="date-input-v-container">
+						<div className="date-input-label-container">
+							<label className={determineLabelClass()}>{dateSelect}</label>
+							<select className="date-input-select">
+								<option value="" disabled selected></option>
+								{months.map((month, index) => (
+									<option key={index} value={month}>
+										{month}
+									</option>
+								))}
+							</select>
+						</div>
+						<div className="date-input-v">V</div>
+					</div>
+				</div>
 			) : null}
 			{dateSelect === "Day" ? (
-				<>
+				<div
+					className={determineDivClass()}
+					onFocus={handleFocus}
+					onBlur={handleBlur}
+				>
 					{" "}
-					<label className="date-input-date-select">{dateSelect}</label>
-					<select ref={selectRef} className="date-input-select">
-						{months.map((month, index) => (
-							<option key={index} value={month}>
-								{month}
-							</option>
-						))}
-					</select>
-				</>
+					<div className="date-input-v-container">
+						<div className="date-input-label-container">
+							<label className={determineLabelClass()}>{dateSelect}</label>
+							<select className="date-input-select">
+								<option value="" disabled selected></option>
+								{days.map((day) => (
+									<option key={day} value={day}>
+										{day}
+									</option>
+								))}
+							</select>
+						</div>
+						<div className="date-input-v">V</div>
+					</div>
+				</div>
 			) : null}
 			{dateSelect === "Year" ? (
-				<>
+				<div
+					className={determineDivClass()}
+					onFocus={handleFocus}
+					onBlur={handleBlur}
+				>
 					{" "}
-					<label className="date-input-date-select">{dateSelect}</label>
-					<select ref={selectRef} className="date-input-select">
-						{months.map((month, index) => (
-							<option key={index} value={month}>
-								{month}
-							</option>
-						))}
-					</select>
-				</>
+					<div className="date-input-v-container">
+						<div className="date-input-label-container">
+							<label className={determineLabelClass()}>{dateSelect}</label>
+							<select className="date-input-select">
+								<option value="" disabled selected></option>
+								{years.map((year) => (
+									<option key={year} value={year}>
+										{year}
+									</option>
+								))}
+							</select>
+						</div>
+						<div className="date-input-v">V</div>
+					</div>
+				</div>
 			) : null}
 		</div>
 	);
